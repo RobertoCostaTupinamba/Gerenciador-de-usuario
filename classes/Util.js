dfe38c4812d50a49460b68752fd32759f1750233
RobertoCostaTupinamba <robertotupinamba@gmail.com>
2020-03-16 16:22:56 -0300

    Arrumando as horas

--- classes/Util.js
+++ classes/Util.js
@@ -1,11 +1,13 @@
 class Utils {
   static dateFormat(date) {
-    let minute, hours;
+    let minute = date.getMinutes(),
+      hours = date.getHours();
     if (date.getMinutes() < 0) {
       minute = "0" + date.getMinutes();
     }
     if (date.getHours() < 10) {
       hours = "0" + date.getHours();
+      console.log();
     }
 
     return (
