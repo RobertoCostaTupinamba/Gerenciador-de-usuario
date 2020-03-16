4f089e8332b84cba724d9c78c12b61e905fd1ad9
RobertoCostaTupinamba <robertotupinamba@gmail.com>
2020-03-16 16:41:34 -0300

    Adicionando evento de click no menu

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ let userController = new UserController(
   "form-user-update",
   "table-users"
 );
+
+document.getElementById("sidebar-menu").addEventListener("click", () => {
+  let menuAtive = document.querySelector(".sidebar-collapse");
+  if (!menuAtive) {
+    document.querySelector("body").classList.add("sidebar-collapse");
+  } else {
+    document.querySelector("body").classList.remove("sidebar-collapse");
+  }
+});
